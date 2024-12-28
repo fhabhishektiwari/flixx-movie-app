d6658f1e56f7f8a3772db0d98b71bf401157fff2
AbhishekTiwari <abhishek.tiwari@firsthive.com>
2024-12-28 17:24:30 +0530

    displaying popular movies on home page

--- js/script.js
+++ js/script.js
@@ -13,6 +13,41 @@ function highlightActiveLink() {
   });
 }
 
+async function displayPopularMovies() {
+  const movieContainer = document.querySelector(".movie-container");
+  const { results } = await fetchAPIData("/movie/popular");
+  console.log(results);
+  results.forEach((movie) => {
+    const div = document.createElement("div");
+    div.classList.add("card");
+    div.innerHTML = `
+          <a href="movie-details.html?id=${movie.id}">
+            ${
+              movie.poster_path
+                ? `<img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" class="card-img-top" alt="${movie.title}">`
+                : `<img src="../images/no-image.jpg" class="card-img-top" alt="${movie.title}">`
+            }
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">${movie.title}</h5>
+            <p class="card-text">
+              <small class="text-muted">Release: ${movie.release_date}</small>
+            </p>
+          </div>
+  `;
+    document.querySelector("#popular-movies").appendChild(div);
+  });
+}
+
+// fetch data from TMDB API
+async function fetchAPIData(endPoint) {
+  const API_KEY = "3967057704130d3993a0864119ce43bd";
+  const API_URL = "https://api.themoviedb.org/3";
+  const response = await fetch(`${API_URL}${endPoint}?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data;
+}
+
 //init app
 function init() {
   //get current page
@@ -20,7 +55,7 @@ function init() {
   switch (currentPage) {
     case "/":
     case "/index.html":
-      console.log("Home page");
+      displayPopularMovies();
       break;
     case "/shows.html":
       console.log("Shows page");
