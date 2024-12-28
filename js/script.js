5f56e581d5fa5f733c3619ed8e59b2f7507602c5
AbhishekTiwari <abhishek.tiwari@firsthive.com>
2024-12-28 16:44:27 +0530

    highlighted the active link

--- js/script.js
+++ js/script.js
@@ -1,1 +1,43 @@
-console.log("initial commit");
+const global = { currentPage: window.location.pathname };
+
+// highlight active link
+function highlightActiveLink() {
+  const links = document.querySelectorAll(".nav-link");
+  links.forEach((link) => {
+    // console.log(link.getAttribute("href"));
+    if (link.getAttribute("href") === global.currentPage) {
+      link.classList.add("active");
+    } else {
+      link.classList.remove("active");
+    }
+  });
+}
+
+//init app
+function init() {
+  //get current page
+  const currentPage = global.currentPage;
+  switch (currentPage) {
+    case "/":
+    case "/index.html":
+      console.log("Home page");
+      break;
+    case "/shows.html":
+      console.log("Shows page");
+      break;
+    case "/movie-details.html":
+      console.log("movie page");
+      break;
+    case "/tv-details.html":
+      console.log("tv page");
+      break;
+    case "/search.html":
+      console.log("search page");
+      break;
+    default:
+      console.log("Page not found");
+  }
+  highlightActiveLink();
+}
+
+document.addEventListener("DOMContentLoaded", init);
